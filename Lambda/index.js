1b58ab44fb4f07170aaf9131b6ff79b2d478ec8c
mythcard <mythcard@gmail.com>
2018-03-06 20:37:15 -0600

    second level commit

--- Lambda/index.js
+++ Lambda/index.js
@@ -4,13 +4,13 @@ const Alexa = require('alexa-sdk');
 
 const request = require('request');
 
-const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL)
+const APP_ID = 'amzn1.ask.skill.3677d8ab-89b4-41c9-8ba8-e74bf1848593';  // TODO replace with your app ID (OPTIONAL)
 
 
 const languageStrings = {
     'en': {
         translation: {
-            SKILL_NAME: 'Space Facts',
+            SKILL_NAME: 'News Blabber',
             HELP_MESSAGE: 'You can say tell me a space fact, or, you can say exit... What can I help you with?',
             HELP_REPROMPT: 'What can I help you with?',
             STOP_MESSAGE: 'Goodbye!',
@@ -26,32 +26,54 @@ const handlers = {
     'sayHi': function () {
         const message = "I am told to say hello, as if you are something special! If you want something, I am listening.";
         const speechOutput = message;
-        this.emit(':tellWithCard', speechOutput, "newsBlabber", speechOutput);
+        this.emit(':askWithCard', speechOutput, "newsBlabber", speechOutput);
     },
-     'breakingNews': function () {
+     'getNews': function () {
+        const message = "Check to see if the intent is working!";
+        const speechOutput = "Please enter the 5 digit pin";
+        //this.emit(':tell', message);
+        this.emit(':askWithCard', speechOutput, "newsBlabber", speechOutput);
+    },
+    'getFiveDigitPin': function () {
+        var message = "Please Enter your first name";
         
-        var url1 = 'https://newsapi.org/v2/top-headlines?' +
-          'country=us&' +
-          'apiKey=380e9f61582d4448b3e26686e4572213';
-          
-       const options = {  
-    url: url1,
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json',
-        'Accept-Charset': 'utf-8'
-    }
-};
-
-request(options, function(err, res, body) {  
-    let json = JSON.parse(body);
-    console.log(json);
-});
-         
-         
-        const message = "Check the log to see if it is working!";
-        const speechOutput = message;
-        this.emit(':tellWithCard', speechOutput, "newsBlabber", speechOutput);
+        
+        console.log("Event details as follows: ",JSON.stringify(this.event));
+        console.log("Context details as follows: ",JSON.stringify(this.context));
+        
+       // console.log("The number that user input is as follows: ", this.event.request.intent.slots.pinValue.value);
+       
+       var str1 = "The pin you entered is :"+this.event.request.intent.slots.pinValue.value;
+        
+       message = str1 + message;
+        
+        this.emit(':ask', message);
+    },
+    'getFirstName': function () {
+        const message = "Check to see if the first Name is being input!";
+        
+        
+        console.log("Event details as follows: ",JSON.stringify(this.event));
+        console.log("Context details as follows: ",JSON.stringify(this.context));
+        
+       // console.log("The number that user input is as follows: ", this.event.request.intent.slots.pinValue.value);
+        
+       
+        
+        this.emit(':ask', message);
+    },
+     'getLastName': function () {
+        const message = "Check to see if the last Name is being input!";
+        
+        
+        console.log("Event details as follows: ",JSON.stringify(this.event));
+        console.log("Context details as follows: ",JSON.stringify(this.context));
+        
+       // console.log("The number that user input is as follows: ", this.event.request.intent.slots.pinValue.value);
+        
+       
+        
+        this.emit(':tell', message);
     },
     'AMAZON.HelpIntent': function () {
         const speechOutput = this.t('HELP_MESSAGE');
